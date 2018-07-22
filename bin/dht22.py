d079c496bd4b4db22e8aac1c8e092340af5b5e67
alexander.russell@sap.com <alexander.russell@sap.com>
2018-07-22 14:56:15 +0200

    Return humidity and temperature as 0 if the DHT sensor is not working.

--- bin/dht22.py
+++ bin/dht22.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 import sys
 import Adafruit_DHT
+import syslog
 
 pin = 24
 humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, pin)
-if humidity is not None and temperature is not None:
-    print('{0:0.1f} {1:0.1f}'.format(temperature, humidity))
-else:
-    print('Failed to get reading. Try again!')
-    sys.exit(1)
-
+if humidity is None or temperature is None:
+    humidity = 0
+    temperature = 0
+    syslog.syslog("Unable to read temperature and humidity")
+print('{0:0.1f} {1:0.1f}'.format(temperature, humidity))
